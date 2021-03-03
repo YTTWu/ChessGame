28083d9a86b3061afd70c18857e62ba52ef66338
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:50:22 -0800

    Update pawn.cpp

--- pawn.cpp
+++ pawn.cpp
@@ -3,6 +3,16 @@
 #include "Pawn.hpp"
 #include "PieceVisitor.hpp"
 
+Pawn::Pawn(int x, int y, char color, char name)
+{
+    set_X(x);
+    set_Y(y);
+    set_color(color);
+    set_name(name);
+}
+
+
+
 bool Pawn::accept(Visitor *pV, char d_x, char d_y)
 {
     if(pV->visitPawn(this, d_x, d_y))

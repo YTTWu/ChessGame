60b3a75fe110ee6724d862664f10f85166f7ce9a
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:08:11 -0800

    Update bishop.cpp

--- bishop.cpp
+++ bishop.cpp
@@ -1,38 +1,6 @@
 
 #include "Bishop.hpp"
 #include "PieceVisitor.hpp"
-Bishop::Bishop(char d_x, char d_y, char p_color) : b_X(d_x), b_Y(d_y), color(p_color)
-{
-    this->name = 'B';
-}
-
-Bishop::~Bishop(){}
-
-void Bishop::setBishop_X(char x)
-{
-    this->b_X = x;
-}
-
-void Bishop::setBishop_Y(char y)
-{
-    this->b_Y = y;
-}
-
-
-char Bishop::getBishop_X()
-{
-    return b_X;
-}
-
-char Bishop::getBishop_Y()
-{
-    return b_Y;
-}
-
-char Bishop::getBishop_Color()
-{
-    return color;
-}
 
 bool Bishop::accept(Visitor *pV, char d_x, char d_y)
 {

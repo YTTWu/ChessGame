29612edbdcb0bb38d5a439fc5f6d4e2dfac48234
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:46:11 -0800

    Update pawn.hpp

--- pawn.hpp
+++ pawn.hpp
@@ -5,10 +5,12 @@
 
 #include <stdio.h>
 #include "ChessPiece.hpp"
+#include "visitor.hpp"
 
 class Pawn : public ChessPiece
 {   
 public:
+    Pawn(int x, int y, char color, char name);
     bool accept(Visitor *vP, char d_x, char d_y);
     
 };

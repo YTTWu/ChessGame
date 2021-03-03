3b6004ed25e68e9f40271c525fce3f2369bd5efe
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:57:33 -0800

    Update bishop.cpp

--- bishop.cpp
+++ bishop.cpp
@@ -1,6 +1,14 @@
 
 #include "Bishop.hpp"
-#include "PieceVisitor.hpp"
+#include "visitor.hpp"
+
+Bishopt::Bishop(int x, int y, char color, char name)
+{
+    set_X(x);
+    set_Y(y);
+    set_color(color);
+    set_name(name);
+}
 
 bool Bishop::accept(Visitor *pV, char d_x, char d_y)
 {

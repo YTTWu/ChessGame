0ee942bfe9acf796096ed2c4f5a8eae39084e4af
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:09:11 -0800

    Update knight.hpp

--- knight.hpp
+++ knight.hpp
@@ -9,28 +9,8 @@
 
 
 class Knight : public ChessPiece
-{
-    
-private:
-    int kn_X;
-    int  kn_Y;
-    char color;
-    char name;
-    
+{   
 public:
-    Knight(int d_x, int d_y, char p_color);
-    
-    ~Knight();
-    
-    bool move(char d_hor, char d_ver);
-    
-    void setKnight_X(int x);
-    void setKnight_Y(int y);
-    
-    char get_X();
-    char get_Y();
-    char get_Color();
-    
     bool accept(Visitor *pV);
     
     

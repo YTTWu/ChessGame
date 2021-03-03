1fbee0bb31098bd2f34f9a2e6acf8e8dc4db6c9d
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:10:24 -0800

    Update pawn.hpp

--- pawn.hpp
+++ pawn.hpp
@@ -7,28 +7,8 @@
 #include "ChessPiece.hpp"
 
 class Pawn : public ChessPiece
-{
-private:
-    char p_X;
-    char p_Y;
-    char color;
-    bool isMoved;
-    char name;
-    
+{   
 public:
-    Pawn(char x, char y, char color);
-    ~Pawn();
-    
-    bool move(char d_hor, char d_ver);
-    
-    void setPawn_X(char x);
-    void setPawn_Y(char y);
-    
-    char getPawn_X();
-    char getPawn_Y();
-    char getPawn_Color();
-    bool getPawn_isMoved();
-    
     bool accept(Visitor *vP, char d_x, char d_y);
     
 };

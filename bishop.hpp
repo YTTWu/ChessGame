8952486a18dee859536deb8694ef44e924646c3c
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:07:31 -0800

    Update bishop.hpp

--- bishop.hpp
+++ bishop.hpp
@@ -8,23 +8,7 @@
 
 class Bishop : public ChessPiece
 {
-private:
-    char b_X;
-    char b_Y;
-    char color;
-    char name;
-public:
-    Bishop(char x, char y, char p_color);
-    ~Bishop();
-    bool move(char d_x, char  d_y);
-    
-    void setBishop_X(char x);
-    void setBishop_Y(char y);
-    
-    char getBishop_X();
-    char getBishop_Y();
-    char getBishop_Color();
-    
+public:    
     bool accept(Visitor *pV, char d_x, char d_y);
 };
 

dcab0d64a0c14ead96a6833cfc498236760ca08e
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:44:55 -0800

    Update knight.cpp

--- knight.cpp
+++ knight.cpp
@@ -1,15 +1,29 @@
 
 
 #include "Knight.hpp"
+#include "visitor.hpp"
+
+Knight::Knight(int x, int y, char color, char name)
+{
+    set_X(x);
+    set_Y(y);
+    set_color(color);
+    set_name(name);
+}
+    
+
 
 bool Knight::accept(Visitor *pV, char d_x, char d_y)
 {
-    
-    if(pV->visitKnight(this, d_x, d_y))
+    bool Knight::accept(Visitor *pV, int d_x, int d_y)
     {
-        return true;
+    
+        if(pV->visitKnight(this, d_x, d_y))
+        {
+            return true;
+        }
+        return false;
     }
-    return false;
 }
 
 

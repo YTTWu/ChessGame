e98e09595a93bd8d75e90360ae22e56ad28ef6bf
Yuteng Terrence Wu <yutengwu@yahoo.com>
2021-03-02 21:08:41 -0800

    Update knight.cpp

--- knight.cpp
+++ knight.cpp
@@ -2,39 +2,6 @@
 
 #include "Knight.hpp"
 
-Knight::Knight(int d_x, int d_y, char p_color) : kn_X(d_x), kn_Y(d_y), color(p_color)
-{
-    this->name = 'K';
-}
-
-Knight::~Knight(){}
-
-void Knight::set_X(char x)
-{
-    this->kn_X = x;
-}
-
-void Knight::set_Y(char y)
-{
-    this->kn_Y = y;
-}
-
-
-char Knight::get_X()
-{
-    return kn_X;
-}
-
-char Knight::get_Y()
-{
-    return kn_Y;
-}
-
-char Knight::get_Color()
-{
-    return color;
-}
-
 bool Knight::accept(Visitor *pV, char d_x, char d_y)
 {
     
